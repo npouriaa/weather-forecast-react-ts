81c29d1995e83804b9cf7ce6085c2619dbf69114
PouriaNavipour <111690110+PouriaNavipour@users.noreply.github.com>
2024-08-25 18:08:49 +0330

    addfooter

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,18 @@ import "./App.css";
 import CurrentStatus from "./components/CurrentStatus";
 import DaysDetails from "./components/daysDetails/DaysDetails";
 import Search from "./components/Search";
-
 const App = () => {
   return (
     <div className="app">
       <Search />
       <CurrentStatus />
       <DaysDetails />
+      <footer>
+        Made by{" "}
+        <a target="_blank" href="https://www.linkedin.com/in/npouriaa">
+          Npouiaa
+        </a>
+      </footer>
     </div>
   );
 };
